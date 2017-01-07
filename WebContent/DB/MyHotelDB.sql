21f7bc138adb533da8b602defd37f126ec7c2acc
Alcris24 <alecriscuolo@alice.it>
2017-01-07 11:06:27 +0100

    Popolamento DB

--- WebContent/DB/MyHotelDB.sql
+++ WebContent/DB/MyHotelDB.sql
@@ -63,3 +63,20 @@ totale double NOT NULL,
 FOREIGN KEY (email) REFERENCES utente(email) ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY(numerocamera) REFERENCES camera(numerocamera) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+Insert into utente(email, password, ruolo, nome, cognome, data_nascita)
+    Values('alecriscuolo@alice.it', 'lovenapoli', 'normale', 'Alessandro', 'Criscuolo', '1995-06-24');
+Insert into utente(email, password, ruolo, nome, cognome, data_nascita)
+    Values('gianlucacriscuolo@alice.it', 'glovenapoli', 'normale', 'Gianluca', 'Criscuolo', '1989-05-14');
+Insert into camera(numerocamera, prezzo, tipologia, immagine, descrizione)
+    Values('24', '250.25', 'Doppia', 'http://www.hotelgalileopadova.it/resources/images/9d880762-0f9b-42e7-9379-d64e90526f10/it/B/camere-a-padova.jpg', 'Camera doppia,televisore, bagno con doccia e yacuzzi.');
+Insert into servizio(nomeservizio, descrizione)
+	Values('wifi', 'Con questa opzione si potrà avere libero accesso all utilizzo della rete wifi dell hotel');
+Insert into ha(numerocamera, nomeservizio)
+	Values('24','wifi');
+Insert into prenotazione(idprenotazione, email, numerocamera, totale, datainizio, datafine)
+	Values('2056', 'alecriscuolo@alice.it', '24', '250.25', '2017-02-15', '2017-02-20');
+Insert into comprende(nomeservizio, idprenotazione)
+	Values('wifi','2056');
+Insert into hacarrello(email, numerocamera, datainizio, datafine, totale)
+	Values('alecriscuolo@alice.it', '24', '2017-02-15', '2017-02-20', '250.25');
