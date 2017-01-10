bb622b62513facf4efbafbd47d989b8f37796970
marcus9522 <marcus9522@gmail.com>
2017-01-10 15:04:34 +0100

    Aggiunto metodo info3 per la pagina modificacamera

--- WebContent/javascript/info.js
+++ WebContent/javascript/info.js
@@ -4,8 +4,6 @@ function info(email){
 function info2(email){
 	window.location.href = "utente?action=getusers&email="+email;  
 }
-function image(){
-	var a= document.getElementById(image).value;
-	console.log(a);
-	document.getElementById("image").src=a;
+function info3(numerocamera){
+	window.location.href = "camera?action=getcamera&numerocamera="+numerocamera;  
 }
